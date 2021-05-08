ce4cb835cdcabc8ec566980e2a675f14910cf056
lujiejie <lujiejie@bixin.cn>
2021-05-08 11:34:35 +0800

    remove shadow in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,11 +7,17 @@ const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
       {/* Rest of the app comes ABOVE the action button component !*/}
-      <Fabs.ActionButton buttonColor='rgba(231,76,60,1)'>
+      <Fabs.ActionButton
+        buttonColor='rgba(231,76,60,1)'
+        fixNativeFeedbackRadius={true}
+      >
         <Fabs.ActionButtonItem
           buttonColor='#9b59b6'
           title='New Task'
+          elevation={0}
           onPress={() => console.log('notes tapped!')}
+          fixNativeFeedbackRadius={true}
+          hideShadow
         >
           <Icon name='md-create' style={styles.actionButtonIcon} />
         </Fabs.ActionButtonItem>
@@ -19,6 +25,8 @@ const App = () => {
           buttonColor='#3498db'
           title='Notifications'
           onPress={() => {}}
+          fixNativeFeedbackRadius={true}
+          hideShadow
         >
           <Icon name='md-notifications' style={styles.actionButtonIcon} />
         </Fabs.ActionButtonItem>
@@ -26,6 +34,8 @@ const App = () => {
           buttonColor='#1abc9c'
           title='All Tasks'
           onPress={() => {}}
+          fixNativeFeedbackRadius={true}
+          hideShadow
         >
           <Icon name='md-checkmark-done' style={styles.actionButtonIcon} />
         </Fabs.ActionButtonItem>
